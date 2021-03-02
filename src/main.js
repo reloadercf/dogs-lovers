1793363e20779be57105711cd905f99e0f8a7671
reloadercf <elcfm1@gmail.com>
2021-03-02 13:20:36 -0600

    cambios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("Raul Adri Carlos")
+import perritos from './data.js'
+
+let elDOM=document.getElementById("app")
+
+
+perritos.dogs.forEach(unperrito=>{
+    let unsolDiv=document.createElement("div")
+    unsolDiv.innerHTML=setHtml(unperrito)
+    elDOM.appendChild(unsolDiv)
+})
+
+function setHtml(unperro){
+    let html=`<img src="${unperro.image}"/>
+            <p>${unperro.breed}</p>
+                `
+    return html
+}
